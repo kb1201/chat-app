c3cf2b00f468126e7269df3addf1064fd5b13b45
Katarina Bošnjak <katarina.bosnjak.external@t.ht.hr>
2025-05-09 19:40:22 +0200

    remove unnecessary code from db and dokcer compose name change

--- infra/db/create-dbs.sh
+++ infra/db/create-dbs.sh
@@ -20,39 +20,6 @@ function create_user_and_database() {
 EOSQL
 }
 
-# Function to create and fill chat tables
-function create_and_fill_chat_tables() {
-  echo "Creating tables in chat"
-  psql -v ON_ERROR_STOP=1 --username "chat"  <<-EOSQL
-
-
-
-    CREATE TABLE IF NOT EXISTS rooms
-    (
-        id uuid PRIMARY KEY,
-        name TEXT NOT NULL,
-        owner_id uuid REFERENCES users(id)
-    );
-
-    CREATE TABLE IF NOT EXISTS room_members
-    (
-        room_id uuid REFERENCES rooms(id),
-        user_id uuid NOT NULL,  -- The user who is a member of the room
-        PRIMARY KEY (room_id, user_id)
-    );
-
-    CREATE TABLE IF NOT EXISTS messages
-    (
-        id uuid PRIMARY KEY,
-        content TEXT NOT NULL,
-        created TIMESTAMP NOT NULL DEFAULT NOW(),
-        user_id uuid NOT NULL,
-        room_id uuid REFERENCES rooms(id)
-    );
-
-EOSQL
-}
-
 # Main execution block for multiple database creation
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
   echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
